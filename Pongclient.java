a9ec04e45744719aa6e224a312f87eb171a67a2e
kjwfds <ravineshn100@gmail.com>
2024-09-06 10:42:16 +1200

    first commit

---
+++ Pongclient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pongmult;
+
+/**
+ *
+ * @author c3rea
+ */
+public class Pongclient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
